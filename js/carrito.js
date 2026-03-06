a1b27d62c14835d82b45c447b1ed1a3efb78b1e0
Bautista Mazzolla <bautistamazzolla4@gmail.com>
2026-03-06 14:32:01 -0300

    carrito

--- js/carrito.js
+++ js/carrito.js
@@ -8,16 +8,48 @@ function rendercarrito(cartitems){
 
     cartitems.forEach(producto=>{
 
-        const card = document.createElement("div")
+    const card = document.createElement("div")
 
-        card.innerHTML = `
-        <h3>${producto.nombre}</h3>
-        <h4>${producto.precio}</h4>
-        `
+    card.innerHTML = `
+    <div class="cart-card">
+        <img src="${producto.img}" width="80">
+        <div class="cart-info">
+            <h3>${producto.nombre}</h3>
+            <h4>$${producto.precio}</h4>
+        </div>
+    </div>
+    `
 
-        cartsection.appendChild(card)
+    cartsection.appendChild(card)
 
-    })
+})
 }
 
 rendercarrito(cartproducts)
+
+function calcularTotal(cartitems){
+    let total = 0
+
+    cartitems.forEach(producto=>{
+        total += producto.precio
+    })
+
+    return total
+}
+
+let total = calcularTotal(cartproducts)
+
+let totalHTML = document.createElement("h2")
+totalHTML.innerText = "Total: $" + total
+
+cartsection.appendChild(totalHTML)
+
+let vaciarBtn = document.createElement("button")
+vaciarBtn.innerText = "Vaciar carrito"
+
+vaciarBtn.onclick = () => {
+    localStorage.removeItem("cartproducts")
+    location.reload()
+}
+
+cartsection.appendChild(vaciarBtn)
